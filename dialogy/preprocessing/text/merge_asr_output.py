aecb6f2de5688c098a32d68df9c1807057646785
Amresh Venugopal <amresh.venugopal@gmail.com>
2020-12-14 00:45:23 +0530

    update: simplify helper functions.

--- dialogy/preprocessing/text/merge_asr_output.py
+++ dialogy/preprocessing/text/merge_asr_output.py
@@ -1,12 +1,12 @@
 from typing import List, Dict, Callable
 
 
-def merge_asr_output(get_utterance: Callable, set_input: Callable):
+def merge_asr_output(access: Callable, mutate: Callable):
     def inner(workflow):
-        utterances: List[List[Dict]] = get_utterance(workflow)
+        utterances: List[List[Dict]] = access(workflow)
         flat_representation = [alternatives["transcript"]
                                for utterance in utterances for alternatives in utterance]
         merged_string = "<s> " + \
             " </s> <s> ".join(flat_representation) + " </s>"
-        set_input(workflow, merged_string)
+        mutate(workflow, merged_string)
     return inner
