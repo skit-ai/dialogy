08a2de830f7a9df4bb7cce310fea31f4bc3a8099
Amresh Venugopal <amresh.venugopal@gmail.com>
2020-12-14 05:10:26 +0530

    update: Plugin abstract class structure.

--- dialogy/plugins/__init__.py
+++ dialogy/plugins/__init__.py
@@ -4,8 +4,5 @@ from dialogy.types.plugins import PluginFn
 
 
 class Plugin(ABC):
-    def __init__(self) -> None:
-        super().__init__()
-
     def exec(self, access: Optional[PluginFn] = None, mutate: Optional[PluginFn] = None) -> None:
         pass
