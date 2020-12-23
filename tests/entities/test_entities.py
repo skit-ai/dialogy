4fc43c38885e9cde384128584d7abddb630abfc6
Aayush Deshraj <aayush@vernacular.ai>
2020-12-23 15:28:35 +0530

    refactor: add numerical entity subtypes and validators

--- tests/entities/test_entities.py
+++ tests/entities/test_entities.py
@@ -1,8 +1,18 @@
 """
 Tests for entities
 """
+import pytest
 
-from dialogy.types.entities import BaseEntity
+from dialogy.types.entities import (BaseEntity,
+                                    NumericalEntity,
+                                    PeopleEntity,
+                                    DateEntity,
+                                    TimeEntity,
+                                    DatetimeEntity,
+                                    TimeIntervalEntity,
+                                    DateIntervalEntity,
+                                    DatetimeIntervalEntity,
+                                    LocationEntity)
 from dialogy.workflow import Workflow
 
 def mock_plugin(_: Workflow) -> None:
@@ -22,3 +32,138 @@ def test_entity_parser():
     entity.add_parser(mock_plugin)
 
     assert entity.parsers == ["mock_plugin"]
+
+
+def test_numerical_entity_type_not_str_error():
+    body = "12 december"
+    with pytest.raises(TypeError):
+        _ = NumericalEntity(
+            range={
+                "from": 0,
+                "to": len(body)
+            },
+            body = body,
+            entity_type = "pattern",
+            value = body,
+            type = 0
+        )
+
+
+def test_people_entity_unit_not_str_error():
+    body = "12 people"
+    with pytest.raises(TypeError):
+        _ = PeopleEntity(
+            range={
+                "from": 0,
+                "to": len(body)
+            },
+            body = body,
+            entity_type = "people",
+            value = body,
+            unit = 0
+        )
+
+
+def test_time_entity_grain_not_str_error():
+    body = "12 pm"
+    with pytest.raises(TypeError):
+        _ = TimeEntity(
+            range={
+                "from": 0,
+                "to": len(body)
+            },
+            body = body,
+            entity_type = "time",
+            value = body,
+            grain = 0
+        )
+
+
+def test_date_entity_type_not_str_error():
+    body = "12 december"
+    with pytest.raises(TypeError):
+        _ = DateEntity(
+            range={
+                "from": 0,
+                "to": len(body)
+            },
+            body = body,
+            entity_type = "date",
+            value = body,
+            grain = 0
+        )
+
+
+def test_datetime_entity_grain_not_str_error():
+    body = "25 december 12 pm"
+    with pytest.raises(TypeError):
+        _ = DatetimeEntity(
+            range={
+                "from": 0,
+                "to": len(body)
+            },
+            body = body,
+            entity_type = "datetime",
+            value = body,
+            grain = 0
+        )
+
+
+def test_time_interval_entity_value_not_dict_error():
+    body = "from 4 pm to 12 am"
+    with pytest.raises(TypeError):
+        _ = TimeIntervalEntity(
+            range={
+                "from": 0,
+                "to": len(body)
+            },
+            body = body,
+            entity_type = "time",
+            value = body,
+            grain = 0
+        )
+
+
+def test_date_interval_entity_value_not_dict_error():
+    body = "from 25 december to 29 december"
+    with pytest.raises(TypeError):
+        _ = DateIntervalEntity(
+            range={
+                "from": 0,
+                "to": len(body)
+            },
+            body = body,
+            entity_type = "date",
+            value = body,
+            grain = 0
+        )
+
+
+def test_datetime_interval_entity_value_not_dict_error():
+    body = "from 25 december 3 pm to 29 december 6 pm"
+    with pytest.raises(TypeError):
+        _ = DatetimeIntervalEntity(
+            range={
+                "from": 0,
+                "to": len(body)
+            },
+            body = body,
+            entity_type = "datetime",
+            value = body,
+            grain = 0
+        )
+
+
+def test_location_entity_value_not_int_error():
+    body = "bangalore"
+    with pytest.raises(TypeError):
+        _ = LocationEntity(
+            range={
+                "from": 0,
+                "to": len(body)
+            },
+            body = body,
+            entity_type = "location",
+            value = body,
+            grain = 0
+        )
