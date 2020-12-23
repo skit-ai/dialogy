7528a00f4ab043587a8adf5d7fa8032641f36594
Aayush Deshraj <aayush@vernacular.ai>
2020-12-23 15:28:35 +0530

    refactor: add numerical entity subtypes and validators

--- dialogy/types/entities/numerical_entities.py
+++ dialogy/types/entities/numerical_entities.py
@@ -18,7 +18,8 @@ class NumericalEntity(BaseEntity):
     Keys:
     - `type` can be either value or interval
     """
-    type = attr.ib(type=str, default='value')
+    type = attr.ib(type=str, default="value",
+                   validator=attr.validators.instance_of(str))
 
 
 @attr.s
@@ -29,18 +30,48 @@ class PeopleEntity(NumericalEntity):
     Keys:
     - `unit` is the type of people (Ex. child, adult, male, female etc)
     """
-    unit = attr.ib(type=str, default=attr.Factory(str))
+    entity_type = attr.ib(type=str, default="people")
+    unit = attr.ib(type=str, default=attr.Factory(str), 
+                   validator=attr.validators.instance_of(str))
 
 
 @attr.s
 class TimeEntity(NumericalEntity):
     """
-    Date and Time Entity Type
+    Time Entity Type
 
     Keys:
     - `grain` tells us whether someone is talking about date or time
     """
-    grain = attr.ib(type=str, default=attr.Factory(str))
+    entity_type = attr.ib(type=str, default="time")
+    grain = attr.ib(type=str, default=attr.Factory(str),
+                    validator=attr.validators.instance_of(str))
+
+
+@attr.s
+class DateEntity(NumericalEntity):
+    """
+    Time Entity Type
+
+    Keys:
+    - `grain` tells us whether someone is talking about date or time
+    """
+    entity_type = attr.ib(type=str, default="date")
+    grain = attr.ib(type=str, default=attr.Factory(str),
+                    validator=attr.validators.instance_of(str))
+
+
+@attr.s
+class DatetimeEntity(NumericalEntity):
+    """
+    Time Entity Type
+
+    Keys:
+    - `grain` tells us whether someone is talking about date or time
+    """
+    entity_type = attr.ib(type=str, default="datetime")
+    grain = attr.ib(type=str, default=attr.Factory(str),
+                    validator=attr.validators.instance_of(str))
 
 
 @attr.s
@@ -51,4 +82,32 @@ class TimeIntervalEntity(TimeEntity):
     Keys:
     - `value` is a Dictionary which has either keys 'from' and 'to' or both
     """
+    entity_type = attr.ib(type=str, default="time")
+    type = attr.ib(type=str, default="interval")
+    value = attr.ib(type=Dict[str, str], default=attr.Factory(Dict), validator=attr.validators.instance_of(Dict))
+
+
+@attr.s
+class DateIntervalEntity(DateEntity):
+    """
+    Datetime Interval Entity Type
+
+    Keys:
+    - `value` is a Dictionary which has either keys 'from' and 'to' or both
+    """
+    entity_type = attr.ib(type=str, default="date")
+    type = attr.ib(type=str, default="interval")
+    value = attr.ib(type=Dict[str, str], default=attr.Factory(Dict), validator=attr.validators.instance_of(Dict))
+
+
+@attr.s
+class DatetimeIntervalEntity(DatetimeEntity):
+    """
+    Datetime Interval Entity Type
+
+    Keys:
+    - `value` is a Dictionary which has either keys 'from' and 'to' or both
+    """
+    entity_type = attr.ib(type=str, default="datetime")
+    type = attr.ib(type=str, default="interval")
     value = attr.ib(type=Dict[str, str], default=attr.Factory(Dict), validator=attr.validators.instance_of(Dict))
