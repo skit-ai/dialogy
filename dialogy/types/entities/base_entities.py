e73468151fd24ae37103ccefaf26f130e3137aa1
Amresh Venugopal <amresh.venugopal@gmail.com>
2021-01-12 16:49:36 +0530

    docs: module, class, method docstrings.

--- dialogy/types/entities/base_entities.py
+++ dialogy/types/entities/base_entities.py
@@ -1,7 +1,11 @@
 """
-Base Entity Type
+Module provides access to a base class to create other entities. 
+This is an unsafe class and not meant for direct use.
+
+classes:
+    - BaseEntity
 """
-from typing import Any, Callable, Dict, List, Optional
+from typing import Any, Dict, List, Optional
 
 import attr
 
@@ -15,41 +19,59 @@ class BaseEntity:
 
     This class is meant for subclassing.
     Do not use it directly in any preprocessing or postprocessing functions.
-    Using it for defining types falls under the intended purposes.
+    Its intended purpose is to define types.
 
     Keys are:
-    - `range` is the character range in the alternative where the entity is parsed
-    - `body` is the string that is extracted
-    - `entity_type` is the type of the entity
-    - `value` is the normalized value of the entity. This can either be a string, an integer or a Dict
+    - `range` is the character range in the alternative where the entity is parsed.
+    - `body` is the string that is extracted.
+    - `entity_type` is the type of the entity.
+    - `value` is the normalized value of the entity. This can either be a string, an integer or a Dict.
     - `parsers` gives the list of all the functions that have changed this entity.
         This list will be in sorted order, which means that the first element has worked
         on the entity first.
-    - `score` is the confidence that the range is the entity
+    - `score` is the confidence that the range is the entity.
     - `alternative_index` is the index of transcript within the ASR output: `List[Utterances]`
         from which this entity was picked up. This may be None.
     """
 
-    range = attr.ib(type=Dict[str, int])
-    body = attr.ib(type=str, validator=attr.validators.instance_of(str))
+    range       = attr.ib(type=Dict[str, int])
+    body        = attr.ib(type=str, validator=attr.validators.instance_of(str))
     entity_type = attr.ib(type=str, validator=attr.validators.instance_of(str))
-    parsers = attr.ib(
+    parsers     = attr.ib(
         type=List[str],
         default=attr.Factory(list),
         validator=attr.validators.instance_of(list),
     )
-    score = attr.ib(type=Optional[float], default=None)
-    alternative_index = attr.ib(type=Optional[int], default=None)
-    latent = attr.ib(type=bool, default=False)
-    # TODO: Make value private
-    values = attr.ib(type=List[Any], default=attr.Factory(list))
+
+    score               = attr.ib(type=Optional[float], default=None)
+    alternative_index   = attr.ib(type=Optional[int], default=None)
+    latent              = attr.ib(type=bool, default=False)
+    values              = attr.ib(type=List[Any], default=attr.Factory(list))
 
     @classmethod
     def __validate(cls, dict_: Dict[str, Any]) -> None:
-        raise TypeError("BaseEntity is not a valid type")
+        """
+        Stub method, raises exception to prevent direct use of this class' instances.
+        
+        Args:
+            dict_ (Dict[str, Any]): Any dictionary with string keys and arbitrarily typed values.
+
+        Raises:
+            NotImplementedError: Always. This method should be overridden in each subclass.
+        """
+        raise NotImplementedError("BaseEntity should not be used directly.")
 
     @classmethod
-    def from_dict(cls, dict_: Dict[str, Any]) -> "BaseEntity":
+    def from_dict(cls, dict_: Dict[str, Any]) -> BaseEntity:
+        """
+        Create an instance of a given class `cls` from a `dict` that complies 
+        with attributes of `cls` through its keys and values. 
+        Compliance is verified using the `__validate` method. It is expected that each subclass 
+        will implement their own flavor of `__validate` to check their respective inputs.
+
+        Returns:
+            BaseEntity: Instance of class
+        """
         cls.__validate(dict_)
         return cls(**dict_)
 
@@ -66,16 +88,18 @@ class BaseEntity:
         self.parsers.append(postprocessor.__name__)
 
     def get_value(self) -> Any:
-        """[summary]
+        """
+        Get value of an entity.
 
-        [extended_summary]
+        The structure of an entity conceals the value in different data structures.
+        This is sugar for access.
 
         Raises:
-            IndexError: [description]
-            KeyError: [description]
+            IndexError: If `values` is not a `list`.
+            KeyError: If each element in `values` is not a `dict`.
 
         Returns:
-            Any: [description]
+            Any: Arbitrary return value, subclasses should override and return specific types.
         """
         try:
             return self.values[0]["value"]
