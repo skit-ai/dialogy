6e8f3c8871052735692616e3ca1fe72a2052edb2
Aayush Deshraj <aayush@vernacular.ai>
2021-01-12 12:14:38 +0530

    feat: add duckling entities

--- dialogy/types/entities/__init__.py
+++ dialogy/types/entities/__init__.py
@@ -3,12 +3,10 @@ Entity Types
 """
 
 from dialogy.types.entities.base_entities import BaseEntity
-from dialogy.types.entities.numerical_entities import (NumericalEntity,
-                                                       PeopleEntity)
-from dialogy.types.entities.datetime_entities import (TimeEntity,
-                                                      DateEntity,
-                                                      DatetimeEntity,
-                                                      TimeIntervalEntity,
-                                                      DateIntervalEntity,
-                                                      DatetimeIntervalEntity)
+from dialogy.types.entities.duckling_entities import (
+    NumericalEntity,
+    PeopleEntity,
+    TimeEntity,
+    TimeIntervalEntity,
+)
 from dialogy.types.entities.location_entities import LocationEntity
