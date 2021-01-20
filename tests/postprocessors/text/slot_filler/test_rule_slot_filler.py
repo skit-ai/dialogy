c56f5c629e2903a8ed9009ca3ce5cab3e4bdcd20
Amresh Venugopal <amresh.venugopal@gmail.com>
2021-01-21 01:43:43 +0530

    test: cases for testing rule-based slot-filler and slots.

---
+++ tests/postprocessors/text/slot_filler/test_rule_slot_filler.py
@@ -0,0 +1,77 @@
+import pytest
+
+from dialogy.postprocessing.text.slot_filler.rule_slot_filler import RuleBasedSlotFillerPlugin
+from dialogy.workflow import Workflow
+from dialogy.types.entities import BaseEntity
+from dialogy.types.intents import Intent
+
+
+def test_rule_slot_filler():
+    rules = {"basic": {"slot_name": "basic_slot", "entity_type": "basic"}}
+    def access(workflow):
+        return workflow.output
+
+    slot_filler = RuleBasedSlotFillerPlugin(rules=rules).exec(access=access)
+    workflow = Workflow(preprocessors=[], postprocessors=[slot_filler])
+    intent = Intent(name="intent", score=0.8)
+
+    body = "12th december"
+    entity = BaseEntity(
+        range={"from": 0, "to": len(body)},
+        body=body,
+        dim="default",
+        type="basic",
+        values=[{"key": "value"}],
+        slot_name="basic_slot"
+    )
+
+    workflow.output = (intent, [entity])
+    workflow.run("")
+    assert workflow.output[0].slots["basic_slot"].values[0] == entity
+
+
+def test_missing_access_fn():
+    rules = {"basic": {"slot_name": "basic_slot", "entity_type": "basic"}}
+
+    slot_filler = RuleBasedSlotFillerPlugin(rules=rules).exec()
+    workflow = Workflow(preprocessors=[], postprocessors=[slot_filler])
+    intent = Intent(name="intent", score=0.8)
+
+    body = "12th december"
+    entity = BaseEntity(
+        range={"from": 0, "to": len(body)},
+        body=body,
+        dim="default",
+        type="basic",
+        values=[{"key": "value"}],
+        slot_name="basic_slot"
+    )
+
+    workflow.output = (intent, [entity])
+
+    with pytest.raises(TypeError):
+        workflow.run("")
+
+
+def test_incorrect_access_fn():
+    rules = {"basic": {"slot_name": "basic_slot", "entity_type": "basic"}}
+    access = 5
+
+    slot_filler = RuleBasedSlotFillerPlugin(rules=rules).exec(access=access)
+    workflow = Workflow(preprocessors=[], postprocessors=[slot_filler])
+    intent = Intent(name="intent", score=0.8)
+
+    body = "12th december"
+    entity = BaseEntity(
+        range={"from": 0, "to": len(body)},
+        body=body,
+        dim="default",
+        type="basic",
+        values=[{"key": "value"}],
+        slot_name="basic_slot"
+    )
+
+    workflow.output = (intent, [entity])
+
+    with pytest.raises(TypeError):
+        workflow.run("")
---
+++ tests/types/slots/test_slot.py
@@ -0,0 +1,31 @@
+from dialogy.types.slots import Slot
+from dialogy.types.entities import BaseEntity
+
+
+def test_slot_build():
+    body = "12th december"
+    entity = BaseEntity(
+        range={"from": 0, "to": len(body)},
+        body=body,
+        dim="default",
+        type="basic",
+        values=[{"key": "value"}],
+    )
+    slot = Slot.fill(entity)
+    assert slot.name == entity.slot_name, "Entity slot_name must match."
+    assert slot.type == [entity.type], "Slot type should be same as entity type."
+    assert slot.values[0] == entity, "Slot value should have 1 item with value same as the entity."
+
+
+def test_slot_entity_addition():
+    body = "12th december"
+    entity = BaseEntity(
+        range={"from": 0, "to": len(body)},
+        body=body,
+        dim="default",
+        type="basic",
+        values=[{"key": "value"}],
+    )
+    slot = Slot.fill(entity)
+    slot.add(entity)
+    assert len(slot.values) == 2
