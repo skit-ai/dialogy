6d222443dcc782637812df6173282d5c5f7d1906
Amresh Venugopal <amresh.venugopal@gmail.com>
2021-01-21 05:53:43 +0530

    docs: explain section.

--- dialogy/constants/__init__.py
+++ dialogy/constants/__init__.py
@@ -8,6 +8,17 @@ Import:
 PREPROCESSORS = "__preprocessors"
 POSTPROCESSORS = "__postprocessors"
 WORKFLOW_PUBLIC_PROPERTIES = [PREPROCESSORS, POSTPROCESSORS]
+
+# ----------------------------------------------------------------
+# This section is needed for dialogy.types.entities.*
+# Each entity type has a few properties which need to be
+# validated for their type.
+#
+# The actual use-case is, a `dict` is received by the factory
+# method of the Entity class, which provides all/enough values to
+# instantiate the Entity. We use this mapping to ensure that the
+# values belong to expected types.
+# ----------------------------------------------------------------
 BASE_ENTITY_PROPS = [
     (["range"], dict),
     (["range", "start"], int),
@@ -20,3 +31,4 @@ BASE_ENTITY_PROPS = [
 
 TIME_ENTITY_PROPS = BASE_ENTITY_PROPS + [(["grain"], str)]
 PEOPLE_ENTITY_PROPS = BASE_ENTITY_PROPS + [(["unit"], str)]
+# ----------------------------------------------------------------
