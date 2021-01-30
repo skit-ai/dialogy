c00d0d90e796ef179335d8fb3d130521baf1337a
Amresh Venugopal <amresh.venugopal@gmail.com>
2021-01-31 03:55:49 +0530

    update: Plugin definition changed. access and mutate are expected at init.

--- dialogy/plugins/__init__.py
+++ dialogy/plugins/__init__.py
@@ -5,30 +5,26 @@ Import classes:
 """
 from abc import ABC
 from typing import Optional, Callable
+
+import attr
 from dialogy.types.plugins import PluginFn
 from dialogy.workflow import Workflow
 
 
-class Plugin(ABC):
-    def exec(
-        self, access: Optional[PluginFn] = None, mutate: Optional[PluginFn] = None
-    ) -> PluginFn:
+@attr.s
+class Plugin:
+    access: Optional[PluginFn] = attr.ib(default=None)
+    mutate: Optional[PluginFn] = attr.ib(default=None)
+
+    def exec(self) -> PluginFn:
         """Compulsory method, each Plugin must have a definite role invoked by `exec`
         everything else (initialization etc) can happen through other methods.
 
         These are required if stateful plugins are needed. This method requires `access` and `mutate`
         functions to interact with the workflow. A workflow can be expected in the closure function.
 
-        example:
-
-        ```python
-        class Plugin(ABC):
-            def exec(self, access: Optional[PluginFn] = None, mutate: Optional[PluginFn] = None) -> None:
-                def plugin(workflow):
-                    pass
-        ```
-
         Args:
             access (Optional[PluginFn], optional): [description]. Defaults to None.
             mutate (Optional[PluginFn], optional): [description]. Defaults to None.
         """
+        ...
