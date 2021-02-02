c709a77d51078c29b2e9ac9b9efdb08d3c732af1
Amresh Venugopal <amresh.venugopal@gmail.com>
2021-02-02 17:51:15 +0530

    docs: for rule_slot_filler.

--- dialogy/postprocess/text/slot_filler/rule_slot_filler.py
+++ dialogy/postprocess/text/slot_filler/rule_slot_filler.py
@@ -1,25 +1,9 @@
 """
-Module provides access to a rule-based slot-filler.
+Module provides access to a rule-based [SlotFiller](../__init__.html).
 
-This is best suited for post-processing.
+Imports:
 
-Consider a minimal rule schema:
-```python
-
-{
-    "<intent_name>": {
-        "<entity_type>": {
-            "slot_name": "<slot_name>",
-            "entity_name": "<entity_name>"
-        } 
-    }
-}
-```
-
-Then irrespective of the entities that are found, only the listed type in the slot shall be present in `values`.
-
-Import Plugin:
-    - SlotFiller
+- RuleBasedSlotFillerPlugin
 """
 from typing import Dict, List, Optional, Tuple
 
@@ -35,45 +19,72 @@ from dialogy.workflow import Workflow
 @attr.s
 class RuleBasedSlotFillerPlugin(Plugin):
     """
-    [summary]
+    An instance of this class is used for generating a slot-filler.
+
+    Schema for rules looks like:
+    ```python
+
+    {
+        "<intent_name>": {
+            "<entity_type>": {
+                "slot_name": "<slot_name>",
+                "entity_name": "<entity_name>"
+            }
+        }
+    }
+    ```
+
+    Plugin signature: 
+
+    - `access(workflow) -> (Intent, List[BaseEntity])`
+    - `mutate` not required since the mutation is implied through Intent structure change.
+
+    Irrespective of the entities that are found, only the listed type in the slot shall be present in `values`.
 
     Attributes:
         - `rules` Dict[str, List[str]]: A relationship between intent and entities.
         - `slots` Dict[str, Slot]: A container for slots for a given intent.
-
-    Raises:
-        TypeError: [description]
-
-    Returns:
-        [type]: [description]
     """
 
     rules = attr.ib(type=Dict[str, Dict[str, str]], default=attr.Factory(Dict))
 
-    def __call__(
-        self, access: Optional[PluginFn] = None, mutate: Optional[PluginFn] = None
-    ) -> PluginFn:
-        def filler(workflow: Workflow) -> None:
-            if access is not None:
+    def filler(self, workflow: Workflow) -> None:
+        """
+        Update an intent slot with compatible entity.
 
-                try:
-                    intent_and_entities: Tuple[Intent, List[BaseEntity]] = access(
-                        workflow
-                    )
-                    intent, entities = intent_and_entities
-                    intent.apply(self.rules)
+        - `access(workflow)` should return a tuple. `Intent`, `List[BaseEntity]`.
+        - Only 1 entity fills the slot type.
 
-                    for entity in entities:
-                        intent.fill_slot(entity)
-                except TypeError as type_error:
-                    raise TypeError(
-                        f"`access` passed to {self.__class__.__name__}"
-                        f" is not a Callable. Received {type(access)} instead."
-                    ) from type_error
-            else:
+        Args:
+            workflow (Workflow)
+
+        Raises:
+            TypeError: self.access should be a Callable.
+        """
+        if self.access is not None:
+
+            try:
+                intent_and_entities: Tuple[Intent, List[BaseEntity]] = self.access(
+                    workflow
+                )
+                intent, entities = intent_and_entities
+                intent.apply(self.rules)
+
+                for entity in entities:
+                    intent.fill_slot(entity)
+            except TypeError as type_error:
                 raise TypeError(
                     f"`access` passed to {self.__class__.__name__}"
-                    f" is not a Callable. Received {type(access)} instead."
-                )
+                    f" is not a Callable. Received {type(self.access)} instead."
+                ) from type_error
+        else:
+            raise TypeError(
+                f"`access` passed to {self.__class__.__name__}"
+                f" is not a Callable. Received {type(self.access)} instead."
+            )
 
-        return filler
+    def __call__(self) -> PluginFn:
+        """
+        [callable-plugin](../../../plugin/plugin.html#__call__)
+        """
+        return self.filler
