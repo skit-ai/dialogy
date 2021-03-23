7c2e77f9667cdee51acbb08ab2b6dc75c8729d89
Amresh Venugopal <amresh.venugopal@gmail.com>
2021-03-23 14:31:23 +0530

    add: overriden set_value method.

--- dialogy/types/entity/time_interval_entity.py
+++ dialogy/types/entity/time_interval_entity.py
@@ -4,9 +4,12 @@ Module provides access to entity types that can be parsed to obtain intervals of
 Import classes:
     - TimeIntervalEntity
 """
+from typing import Any, Dict, Optional
+
 import attr
 
 from dialogy import constants
+from dialogy import constants as const
 from dialogy.types.entity.time_entity import TimeEntity
 
 
@@ -24,4 +27,38 @@ class TimeIntervalEntity(TimeEntity):
     """
 
     origin = "interval"
+    dim = "time"
     __properties_map = constants.TIME_ENTITY_PROPS
+
+    def set_value(self, value: Optional[Dict[str, Any]] = None) -> None:
+        """
+        Set values and value attribute.
+
+        Args:
+            value (Any): The parsed value of an entity token.
+
+        Returns:
+            None
+        """
+        if value is None and isinstance(self.values, list) and len(self.values) > 0:
+            self.value = self.values[0]
+        else:
+            if value:
+                if (
+                    isinstance(value, dict)
+                    and const.TO in value
+                    and const.FROM in value
+                ):
+                    self.values = [value]
+                    self.value = value
+                else:
+                    raise TypeError(
+                        "Expected value to be a dict and look like: {'to': ...,  'from': ...}"
+                        f" but {type(value)} found."
+                    )
+            else:
+                raise TypeError(
+                    "Either values should be a non empty list"
+                    " or value should look like: {'to': ...,  'from': ...}"
+                    f" but {type(value)} found."
+                )
