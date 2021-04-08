07ce55aefeea5657ceffedd0004f5d4b5575cd15
Amresh Venugopal <amresh.venugopal@gmail.com>
2021-04-08 20:09:37 +0530

    lint: cleanup example.

--- dialogy/types/entity/duration_entity.py
+++ dialogy/types/entity/duration_entity.py
@@ -4,7 +4,7 @@ Module provides access to an entity type (class) to handle locations.
 Import classes:
     - LocationEntity
 """
-from typing import Dict, List, Any
+from typing import Any, Dict, List
 
 import attr
 
@@ -18,32 +18,6 @@ class DurationEntity(BaseEntity):
     Use this type for handling locations available with reference-ids.
     This is not meant for (latitude, longitude) values, those will be covered in GeoPointEntity.
     """
+
     normalized = attr.ib(type=Dict[str, Any], default=attr.Factory(dict))
     _meta = attr.ib(type=Dict[str, str], default=attr.Factory(dict))
-
-
-"""
-{
-    "body": "2 days",
-    "latent": false,
-    "values": [
-      {
-        "value": 2,
-        "day": 2,
-        "type": "value",
-        "unit": "day",
-        "normalized": {
-          "value": 172800,
-          "unit": "second"
-        }
-      }
-    ],
-    "type": "duration",
-    "range": {
-      "start": 0,
-      "end": 6
-    },
-    "parser": "rhea",
-    "alternative_index": 0
-}
-"""
