8565175a019dfe252508a5fdd34554266a816acc
Amresh Venugopal <amresh.venugopal@gmail.com>
2021-04-11 07:57:16 +0530

    docs: refrences to module and depth.

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -45,14 +45,14 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+add_module_names = False
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'flask'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs_src/index.rst
+++ docs_src/index.rst
@@ -7,10 +7,10 @@ Welcome to dialogy's documentation!
 ===================================
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 4
    :caption: Contents:
 
-
+   ../source/modules
 
 Indices and tables
 ==================
