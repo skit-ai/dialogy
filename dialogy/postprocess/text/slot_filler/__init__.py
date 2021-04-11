836261a311ef5578ecd528e10b729aaeb7dca95e
Amresh Venugopal <amresh.venugopal@gmail.com>
2021-04-11 07:43:01 +0530

    docs: missing labels error corrected.

--- dialogy/postprocess/text/slot_filler/__init__.py
+++ dialogy/postprocess/text/slot_filler/__init__.py
@@ -8,10 +8,10 @@ To handle a single dialog we need a system that can:
 3. Check for associations between the two.
 
 A slot-filler does the third bit. Once an :ref:`Intent <intent>` and a set of
-:ref:`Entity <BaseEntity>` are identified, there needs to be an arrangement that
+:ref:`Entity <base_entity>` are identified, there needs to be an arrangement that
 tells us if any of these are related to each other.
 
-As per the current design, an :ref:`Intent <intent>` can contain a number of :ref:`Slot <slot>`(s). A :ref:`Slot <slot>`
+As per the current design, an :ref:`Intent <intent>` can contain a number of :ref:`Slot <slot>`. A :ref:`Slot <slot>`
 fills an entity of a pre-defined type.
 
 - A :ref:`Slot <slot>` configured to fill a :ref:`TimeEntity <time_entity>` will not fill a :ref:`LocationEntity <location_entity>`.
