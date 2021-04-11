62dd9d83e157b110f721b57495be35f1b53b637e
Amresh Venugopal <amresh.venugopal@gmail.com>
2021-04-11 06:04:38 +0530

    update: remove entire module name for brevity in docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["constants.py"]
-
+add_module_names = False
 
 # -- Options for HTML output -------------------------------------------------
 
