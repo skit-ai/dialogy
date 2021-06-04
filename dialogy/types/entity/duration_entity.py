f2a0b757831d499a8e28bfd309b0a7a50e3521de
ltbringer <amresh.venugopal@gmail.com>
2021-06-04 14:50:29 +0530

    update: duration entities now return value in seconds.

--- dialogy/types/entity/duration_entity.py
+++ dialogy/types/entity/duration_entity.py
@@ -9,6 +9,7 @@ from typing import Any, Dict
 
 import attr
 
+from dialogy import constants as const
 from dialogy.types.entity import BaseEntity
 
 
@@ -28,3 +29,33 @@ class DurationEntity(BaseEntity):
 
     normalized = attr.ib(type=Dict[str, Any], default=attr.Factory(dict))
     _meta = attr.ib(type=Dict[str, str], default=attr.Factory(dict))
+
+    @classmethod
+    def reshape(cls, dict_: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        :type dict_: Dict[str, Any]
+        """
+        match_start = dict_[const.EntityKeys.START]
+        match_end = dict_[const.EntityKeys.END]
+
+        dict_[const.EntityKeys.RANGE] = {
+            const.EntityKeys.START: match_start,
+            const.EntityKeys.END: match_end,
+        }
+        # ['body', 'start', 'value', 'end', 'dim', 'latent']
+
+        dict_[const.EntityKeys.TYPE] = dict_[const.EntityKeys.DIM]
+
+        # This piece is a preparation for multiple entity values.
+        # So, even though we are confident of the value found, we are still keeping the
+        # structure.
+        value = dict_[const.EntityKeys.VALUE][const.EntityKeys.NORMALIZED]
+        dict_[const.EntityKeys.VALUES] = [value]
+
+        del dict_[const.EntityKeys.START]
+        del dict_[const.EntityKeys.END]
+        del dict_[const.EntityKeys.VALUE]
+        return dict_
+
+    def set_value(self, value: Any = None) -> "BaseEntity":
+        return super().set_value(value=value)
