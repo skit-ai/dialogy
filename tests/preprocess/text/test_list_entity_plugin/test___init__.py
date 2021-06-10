fd49bc8cea92c9c7b2b20d06a0d4796280e3bf5a
ltbringer <amresh.venugopal@gmail.com>
2021-06-11 04:54:59 +0530

    test: remove entity_map.

--- tests/preprocess/text/test_list_entity_plugin/test___init__.py
+++ tests/preprocess/text/test_list_entity_plugin/test___init__.py
@@ -9,34 +9,6 @@ from dialogy.workflow import Workflow
 from tests import EXCEPTIONS, load_tests
 
 
-@attr.s
-class MockCustomEntity(BaseEntity):
-    """
-    Extend BaseEntity for products of any kind.
-    An instance of this class should be used to
-    wrap tokens that are understood as product entities.
-    """
-
-    values = attr.ib(
-        type=List[Dict[str, str]],
-        default=attr.Factory(list),
-        validator=attr.validators.instance_of(List),
-    )
-    _meta = attr.ib(type=Dict[str, str], default=attr.Factory(dict))
-    value: Optional[str] = attr.ib(default=None)
-
-    def __attrs_post_init__(self) -> None:
-        self.entity_type = "mock_entity"
-
-    def set_value(self, keyword: Optional[str] = None):
-        if keyword:
-            value = keyword.lower().strip()
-            self.values = [{"value": value}]
-            self.value = value
-        else:
-            self.value = self.values[0]["value"]
-
-
 class EntMocker:
     def __init__(self, token, label):
         self.text = token
@@ -64,7 +36,7 @@ def mutate(w, v):
 def test_value_error_if_incorrect_style():
     with pytest.raises(ValueError):
         l = ListEntityPlugin(
-            access=lambda w: (w.input,), mutate=mutate, style="unknown", entity_map={}
+            access=lambda w: (w.input,), mutate=mutate, style="unknown"
         )
         l._parse({"location": ["..."]})
 
@@ -75,7 +47,6 @@ def test_value_error_if_spacy_missing():
             access=lambda w: (w.input,),
             mutate=mutate,
             style="spacy",
-            entity_map={},
             spacy_nlp=None,
         )
         l.ner_search("...")
@@ -87,7 +58,6 @@ def test_type_error_if_compiled_patterns_missing():
             access=lambda w: (w.input,),
             mutate=mutate,
             style="spacy",
-            entity_map={},
             spacy_nlp=None,
         )
         l.regex_search("...")
@@ -101,10 +71,6 @@ def test_get_list_entities(payload):
     config = payload.get("config")
     spacy_mocker = None
     transcripts = [expectation["text"] for expectation in input_]
-    entity_class_map = {"MockCustomEntity": MockCustomEntity}
-
-    for entity_type, entity_class_name in config["entity_map"].items():
-        config["entity_map"][entity_type] = entity_class_map[entity_class_name]
 
     if config["style"] == "spacy":
         spacy_mocker = SpacyMocker(input_)
