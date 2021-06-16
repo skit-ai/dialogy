accd1bcbab26b645ae115e17b67b6a7b6b5f9fcf
ltbringer <amresh.venugopal@gmail.com>
2021-06-17 04:11:01 +0530

    refactor: deprecate parser module.

--- dialogy/parser/text/entity/duckling_parser.py
+++
@@ -1,6 +0,0 @@
-"""
-.. deprecated:: DucklingParser will be deprecated, please use DucklingPlugin instead. Please use DucklingPlug
-"""
-from dialogy.preprocess.text.duckling_plugin import DucklingPlugin
-
-DucklingParser = DucklingPlugin
