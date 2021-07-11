3ebb9a37d7323efb8194e797f155407edf8773ea
ltbringer <amresh.venugopal@gmail.com>
2021-07-11 18:46:22 +0530

    add: support for amount-of-money entities from Duckling.

--- dialogy/types/entity/__init__.py
+++ dialogy/types/entity/__init__.py
@@ -6,6 +6,7 @@ will be added depending on open issues.
 """
 
 from dialogy.types.entity.base_entity import BaseEntity, entity_synthesis
+from dialogy.types.entity.currency_entity import CurrencyEntity
 from dialogy.types.entity.duration_entity import DurationEntity
 from dialogy.types.entity.keyword_entity import KeywordEntity
 from dialogy.types.entity.location_entity import LocationEntity
@@ -19,4 +20,5 @@ dimension_entity_map = {
     "people": {"value": PeopleEntity},
     "time": {"value": TimeEntity, "interval": TimeIntervalEntity},
     "duration": {"value": DurationEntity},
+    "amount-of-money": {"value": CurrencyEntity},
 }
