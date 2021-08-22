fba5bba587a9b8877fdf5936fd5de245f2b8e234
ltbringer <amresh.venugopal@gmail.com>
2021-08-22 20:14:50 +0530

    refactor: move WER calibration plugins to dialogy.plugins

--- dialogy/plugins/__init__.py
+++ dialogy/plugins/__init__.py
@@ -2,6 +2,10 @@ from dialogy.plugins.postprocess.text.slot_filler.rule_slot_filler import (
     RuleBasedSlotFillerPlugin,
 )
 from dialogy.plugins.postprocess.text.voting.intent_voting import VotePlugin
+from dialogy.plugins.preprocess.text.calibration import (
+    WERCalibrationConfig,
+    WERCalibrationPlugin,
+)
 from dialogy.plugins.preprocess.text.duckling_plugin import DucklingPlugin
 from dialogy.plugins.preprocess.text.list_entity_plugin import ListEntityPlugin
 from dialogy.plugins.preprocess.text.merge_asr_output import MergeASROutputPlugin
