43a683221ad07a3bdddec57774820d0dbbad4391
ltbringer <amresh.venugopal@gmail.com>
2021-08-29 00:05:14 +0530

    add: save_file method. refactor: rename safe_load to load_file.

--- dialogy/utils/__init__.py
+++ dialogy/utils/__init__.py
@@ -1,3 +1,4 @@
-from dialogy.utils.file_handler import safe_load
+from dialogy.utils.file_handler import load_file, save_file
 from dialogy.utils.misc import dt2timestamp, traverse_dict, validate_type
 from dialogy.utils.normalize_utterance import normalize
+from dialogy.utils.logger import logger
--- tests/utils/test_file_handler.py
+++ tests/utils/test_file_handler.py
@@ -2,26 +2,26 @@ import json
 import os
 import tempfile
 
-from dialogy.utils.file_handler import safe_load
+from dialogy.utils.file_handler import load_file
 
 
-def test_safe_load_json():
+def test_load_file_json():
     data = {"a": "b"}
     _, file_path = tempfile.mkstemp(suffix=".json")
     with open(file_path, "w") as f:
         json.dump(data, f)
-    assert safe_load(file_path, loader=json.load) == data
+    assert load_file(file_path, loader=json.load) == data
     os.remove(file_path)
 
 
 def test_missing_file_path():
-    assert safe_load(None, loader=json.load) is None
+    assert load_file(None, loader=json.load) is None
 
 
-def test_safe_load_file():
+def test_load_file_file():
     data = ""
     _, file_path = tempfile.mkstemp(suffix=".txt")
     with open(file_path, "w") as f:
         f.write(data)
-    assert safe_load(file_path) == data
+    assert load_file(file_path) == data
     os.remove(file_path)
