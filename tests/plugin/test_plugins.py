33c636fb404018e55a43d05ffbf3692d71e8cab3
ltbringer <amresh.venugopal@gmail.com>
2021-08-29 15:08:26 +0530

    add: cases for train and transform methods.

--- tests/plugin/test_plugins.py
+++ tests/plugin/test_plugins.py
@@ -103,6 +103,38 @@ def test_arbitrary_plugin() -> None:
     assert words == ["hello world", "hi world"]
 
 
+def test_arbitrary_plugin_with_debug_mode() -> None:
+    """
+    We will test how an arbitrary-class-based plugin works with a workflow.
+    """
+    # create an instance of `ArbitraryPlugin`.
+    arbitrary_plugin = ArbitraryPlugin(access=access, mutate=mutate, debug=True)
+
+    # create an instance of a `Workflow`.
+    # we are calling the `arbitrary_plugin` to get the `plugin` de method.
+    workflow = Workflow([arbitrary_plugin])
+
+    # This runs all the `preprocessors` and `postprocessors` provided previously.
+    # we can expect our `arbitrary_plugin` will also be used.
+    workflow.run(([2, 5], ["hello", "hi"]))
+
+    numbers, words = workflow.output  # pylint: disable=unpacking-non-sequence
+
+    # This test would pass only if our plugin works correctly!
+    assert numbers == [4, 7]
+    assert words == ["hello world", "hi world"]
+
+
 def test_raw_plugin() -> None:
     plugin = Plugin(access=access, mutate=mutate)
     assert plugin.utility() is None
+
+
+def test_plugin_train() -> None:
+    arbitrary_plugin = ArbitraryPlugin(access=access, mutate=mutate, debug=True)
+    assert arbitrary_plugin.train([]) is None
+
+
+def test_plugin_transform() -> None:
+    arbitrary_plugin = ArbitraryPlugin(access=access, mutate=mutate, debug=True)
+    assert arbitrary_plugin.transform([]) is None
