4c0c59ab032d675b42b5641868265bfef4d3af70
Ayush Shridhar <ayush@Ayushs-MacBook-Air.local>
2021-09-09 12:10:35 +0530

    - Use dataframe directly instead of filenames - CalibrationModel.transform returns filtered dataframe without changing schema

--- dialogy/plugins/text/calibration/xgb.py
+++ dialogy/plugins/text/calibration/xgb.py
@@ -9,8 +9,11 @@ import json
 import math
 import pickle
 import sqlite3
+import traceback
 from typing import Any, Dict, List, Optional, Tuple
 
+from pandas.core.frame import DataFrame
+from dialogy.utils import logger
 import jiwer
 import numpy as np
 import pandas as pd
@@ -36,8 +39,7 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
         cux = sqlite3.connect(fname)
         return pd.read_sql_query("SELECT * FROM DATA", cux)
 
-    def fit(self, filename: str, y=None):
-        df = self.read_sqlite(filename)
+    def fit(self, df: pd.DataFrame, y=None):
         texts = []
         for _, row in tqdm(df.iterrows()):
             real_transcript = json.loads(row["tag"])["text"]
@@ -48,6 +50,7 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
             for alt in alts[0]:
                 texts.append(alt["transcript"])
 
+        logger.debug("Step 1/2: Training vectorizer model")
         self.vectorizer.fit(texts)
         return self
 
@@ -64,8 +67,7 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
             alt["am_score"] / math.sqrt(l),
         ]
 
-    def transform(self, filename: str) -> Tuple[np.ndarray, List]:
-        df = self.read_sqlite(filename)
+    def transform(self, df: pd.DataFrame) -> Tuple[np.ndarray, List]:
         features, targets = [], []
         print(df.head())
         for _, row in tqdm(df.iterrows()):
@@ -85,14 +87,52 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
 
 
 class CalibrationModel(Plugin):
-    def __init__(self) -> None:
+    def __init__(self, threshold) -> None:
         self.extraction_pipeline = FeatureExtractor()
         self.clf = XGBRegressor(n_jobs=4)
+        self.data_column = "data"
+        self.threshold = threshold  # Todo : make configurable
 
     def train(self, filename: str, model_name: str) -> None:
         X, y = self.extraction_pipeline.fit_transform(filename)
+        logger.debug("Step 2/2: Training regressor model")
         self.clf.fit(X, y)
         self.save(model_name)
+
+    def filter_asr_output(self, asr_output: dict) -> dict:
+        alternatives = asr_output["alternatives"]
+        filtered_alternatives = []
+        for alternative in alternatives[0]:
+            if self.inference(alternative) < self.threshold:
+                filtered_alternatives.append(alternative)
+        return {"alternatives": [filtered_alternatives]}
+
+    def transform(self, training_data: pd.DataFrame) -> pd.DataFrame:
+        # filters df alternatives and feeds into merge_asr_output,
+        # doesn't change training_data schema
+        training_data["use"] = True
+        logger.debug("Transforming training data.")
+        for i, row in training_data.iterrows():
+            asr_output = None
+            try:
+                asr_output = json.loads(row[self.data_column])
+                if asr_output:
+                    filtered_asr_output = self.filter_asr_output(asr_output)
+                    training_data.loc[i, self.data_column] = filtered_asr_output
+                else:
+                    training_data.loc[i, "use"] = False
+            except Exception as error:  # pylint: disable=broad-except
+                training_data.loc[i, "use"] = False
+                logger.error(f"{error} -- {asr_output}\n{traceback.format_exc()}")
+
+        training_data_ = training_data[training_data.use].copy()
+        training_data_.drop("use", axis=1, inplace=True)
+        discarded_data = len(training_data) - len(training_data_)
+        if discarded_data:
+            logger.debug(
+                f"Discarding {discarded_data} samples because the alternatives couldn't be parsed."
+            )
+        return training_data_
 
     def inference(self, alternatives: List[Dict]) -> np.ndarray:
         return self.clf.predict(self.extraction_pipeline.features(alternatives))
@@ -102,8 +142,3 @@ class CalibrationModel(Plugin):
 
     def utility(self, *args: Any) -> Any:
         return self.inference(*args)  # pylint: disable=no-value-for-parameter
-
-
-if __name__ == "__main__":
-    c = CalibrationModel()
-    c.train("94.sqlite", "calmodel.pkl")
