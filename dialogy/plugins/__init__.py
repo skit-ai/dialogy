6fa1f094b320c202dcea3eb9ca6c86f4f9d3d2b2
ltbringer <amresh.venugopal@gmail.com>
2021-09-15 02:31:23 +0530

    update: import paths.

--- dialogy/plugins/__init__.py
+++ dialogy/plugins/__init__.py
@@ -1,4 +1,5 @@
 from dialogy.plugins.text.calibration import WERCalibrationConfig, WERCalibrationPlugin
+from dialogy.plugins.text.canonicalization import CanonicalizationPlugin
 from dialogy.plugins.text.classification.xlmr import XLMRMultiClass
 from dialogy.plugins.text.duckling_plugin import DucklingPlugin
 from dialogy.plugins.text.list_entity_plugin import ListEntityPlugin
