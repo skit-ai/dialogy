35953668adad35833b3c63e9fa14bc1aaa7a8141
Kriti Anandan <kriti@vernacular.ai>
2021-10-08 13:33:23 +0530

    Add: lower bound plugin

--- dialogy/plugins/__init__.py
+++ dialogy/plugins/__init__.py
@@ -2,6 +2,7 @@ from dialogy.plugins.text.calibration.xgb import CalibrationModel
 from dialogy.plugins.text.canonicalization import CanonicalizationPlugin
 from dialogy.plugins.text.classification.xlmr import XLMRMultiClass
 from dialogy.plugins.text.duckling_plugin import DucklingPlugin
+from dialogy.plugins.text.lb_plugin import DucklingPluginLB
 from dialogy.plugins.text.list_entity_plugin import ListEntityPlugin
 from dialogy.plugins.text.merge_asr_output import MergeASROutputPlugin
 from dialogy.plugins.text.slot_filler.rule_slot_filler import RuleBasedSlotFillerPlugin
