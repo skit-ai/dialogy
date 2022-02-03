9e6402f640733cee05c0eefca3faca5c8f2a639c
ltbringer <amresh.venugopal@gmail.com>
2022-02-04 00:39:23 +0530

    update: export is_utterance from utils.

--- dialogy/utils/__init__.py
+++ dialogy/utils/__init__.py
@@ -3,4 +3,4 @@ from dialogy.utils.file_handler import create_timestamps_path, load_file, save_f
 from dialogy.utils.logger import logger
 from dialogy.utils.misc import dt2timestamp, traverse_dict, validate_type
 from dialogy.utils.naive_lang_detect import lang_detect_from_text
-from dialogy.utils.normalize_utterance import normalize
+from dialogy.utils.normalize_utterance import normalize, is_utterance
