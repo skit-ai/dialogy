97f6acac3e0818d567832925799ff8e90647f74b
ltbringer <amresh.venugopal@gmail.com>
2022-03-13 18:00:54 +0530

    update: remove case for unexpected branch.

--- tests/plugin/text/classification/test_retain_original_intent.py
+++ tests/plugin/text/classification/test_retain_original_intent.py
@@ -15,8 +15,3 @@ def test_retain_original_intent():
 def test_retain_original_intent_with_no_intents():
     plugin = RetainOriginalIntentPlugin()
     assert plugin.retain([]) == {}
-
-
-def test_retain_original_intent_with_no_intent():
-    plugin = RetainOriginalIntentPlugin()
-    assert plugin.retain(2) == {}
