d7bdfd9d935101b11c4887aeccf47daee8270d6f
ltbringer <amresh.venugopal@gmail.com>
2022-03-29 20:58:15 +0530

    fix: #136.

--- dialogy/workflow/workflow.py
+++ dialogy/workflow/workflow.py
@@ -277,7 +277,12 @@ class Workflow:
         """
         with self.lock:
             self.input = input_
-            return self.execute().flush()
+            try:
+                return self.execute().flush()
+            except Exception as e:
+                raise e
+            finally:
+                self.__reset()
 
     def flush(self) -> Tuple[Dict[str, Any], Dict[str, Any]]:
         """
