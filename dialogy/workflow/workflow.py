56487836db3f91cb3f0a92cf72ec06ea9359537a
Daksh Varshneya <dakshvarshneya@admins-MacBook-Pro-7.local>
2022-08-18 17:21:51 +0530

    fix class name

--- dialogy/workflow/workflow.py
+++ dialogy/workflow/workflow.py
@@ -265,7 +265,7 @@ class Workflow:
             start = time.perf_counter()
             plugin(self)
             end = time.perf_counter()
-            self.set(f"{const.OUTPUT}.{const.RUNTIMES}", {plugin.__class__: round(end - start, 4)})
+            self.set(f"{const.OUTPUT}.{const.RUNTIMES}", {plugin.__class__.__name__: round(end - start, 4)})
             # logs are available only when debug=False during class initialization
             if self.debug:
                 history["after"] = {
