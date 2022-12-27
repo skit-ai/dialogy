7442b24868489e5eb4aaca3bff50039275490dda
arsh <arsh.deep@skit.ai>
2022-12-27 16:11:17 +0530

    added test case for downloading stanza model
    Signed-off-by: arsh <arsh.deep@skit.ai>

--- tests/plugin/text/test_list_search_plugin/test_list_search_plugin.py
+++ tests/plugin/text/test_list_search_plugin/test_list_search_plugin.py
@@ -104,3 +104,36 @@ def test_get_list_entities(payload, mocker):
         assert entity["type"] == expected[i]["type"]
         if "score" in expected[i]:
             assert entity["score"] == expected[i]["score"]
+
+
+def test_get_words_from_nlp_download_model(mocker):
+    # here we are not mocking the model download step
+    l = ListSearchPlugin(
+        dest="output.entities",
+        fuzzy_threshold=0.3,
+        fuzzy_dp_config={
+            "en": {
+                "location": {
+                    "New Delhi": "Delhi",
+                    "new deli": "Delhi",
+                    "delhi": "Delhi",
+                }
+            }
+        },
+    )
+    expected = json.loads(
+        """[[{"id": 1, "text": "I", "lemma": "I", "upos": "PRON", "xpos": "PRP", 
+    "feats": "Case=Nom|Number=Sing|Person=1|PronType=Prs", "head": 2, "deprel": "nsubj", "misc": "", 
+    "start_char": 0, "end_char": 1, "ner": "O"}, {"id": 2, "text": "live", "lemma": "live", 
+    "upos": "VERB", "xpos": "VBP", "feats": "Mood=Ind|Tense=Pres|VerbForm=Fin", "head": 0, "deprel": "root", 
+    "misc": "", "start_char": 2, "end_char": 6, "ner": "O"}, {"id": 3, "text": "in", "lemma": "in", 
+    "upos": "ADP", "xpos": "IN", "head": 4, "deprel": "case", "misc": "", "start_char": 7, "end_char": 9, 
+    "ner": "O"}, {"id": 4, "text": "Delhi", "lemma": "Delhi", "upos": "PROPN", 
+    "xpos": "NNP", "feats": "Number=Sing", "head": 2, "deprel": "obl", "misc": "", 
+    "start_char": 10, "end_char": 16, "ner": "O"}]]"""
+    )
+
+    def mock_nlp(query):
+        return MockDocument(expected)
+
+    assert expected[0] == l.get_words_from_nlp(mock_nlp, "I live in Delhi")
