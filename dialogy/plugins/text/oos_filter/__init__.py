821e5f9d69134a8c5d7ef9f81fce085868b89f63
sanchit-ahuja <sanchitahuja205@gmail.com>
2023-03-07 12:07:09 +0530

    add: oos_filter
    Signed-off-by: sanchit-ahuja <sanchitahuja205@gmail.com>

---
+++ dialogy/plugins/text/oos_filter/__init__.py
@@ -0,0 +1,24 @@
+"""
+.. OOSFilter:
+"""
+
+from typing import Any, Callable, Dict, List, Optional
+from dialogy.base.plugin import Plugin, Input, Output
+from dialogy.types import Intent
+
+
+class OOSFilterPlugin(Plugin):
+    def __init__(
+        self, dest=None, guards=None, threshold=None, intent_oos=None, **kwargs
+    ) -> None:
+        super().__init__(dest=dest, guards=guards, **kwargs)
+        self.threshold = threshold
+        self.intent_oos = intent_oos
+
+    def set_oos_intent(self, intents: List[Intent]) -> Any:
+        if intents[0].score < self.threshold:
+            intents[0].name = self.intent_oos
+        return intents
+
+    def utility(self, input_: Input, output: Output) -> Any:
+        return self.set_oos_intent(output.intents)
