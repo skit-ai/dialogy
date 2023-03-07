59e850b0172925d65a6b3f1c0678b286d2319e00
sanchit-ahuja <sanchitahuja205@gmail.com>
2023-03-07 16:18:07 +0530

    add: few comments
    Signed-off-by: sanchit-ahuja <sanchitahuja205@gmail.com>

--- dialogy/plugins/text/oos_filter/__init__.py
+++ dialogy/plugins/text/oos_filter/__init__.py
@@ -1,5 +1,9 @@
 """
-.. OOSFilter:
+.. OOSFilter: Mutate an incoming intent to out-of-scope intent (OOS) if the score of the predicted intent is less than certain threshold
+
+Expects 2 mandatory arguments:
+1. intent_oos -> str
+2. threshold value -> float
 """
 
 from typing import Any, Callable, Dict, List, Optional
@@ -9,7 +13,12 @@ from dialogy.types import Intent
 
 class OOSFilterPlugin(Plugin):
     def __init__(
-        self, dest=None, guards=None, threshold=None, intent_oos=None, **kwargs
+        self,
+        dest=None,
+        guards=None,
+        threshold: float = None,
+        intent_oos: str = None,
+        **kwargs
     ) -> None:
         super().__init__(dest=dest, guards=guards, **kwargs)
         self.threshold = threshold
