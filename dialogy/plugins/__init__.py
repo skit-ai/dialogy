a969d704577e72e5cc0bfed821ccf07d5625361d
Daksh Varshneya <dakshvar22@gmail.com>
2023-03-21 10:44:31 +0530

    port changes from 0.9.x (#185)

--- dialogy/plugins/__init__.py
+++ dialogy/plugins/__init__.py
@@ -13,3 +13,4 @@ from dialogy.plugins.text.slot_filler.rule_slot_filler import RuleBasedSlotFille
 from dialogy.plugins.text.address_parser import AddressParserPlugin
 from dialogy.plugins.text.error_recovery.error_recovery import ErrorRecoveryPlugin
 from dialogy.plugins.text.oos_filter import OOSFilterPlugin
+from dialogy.plugins.text.qc_plugin import QCPlugin
