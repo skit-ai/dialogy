33662abf4c1c195e15d1aba1bd382bf10e906374
Sanchit Ahuja <30617988+sanchit-ahuja@users.noreply.github.com>
2023-05-12 13:00:31 +0530

    update: update OOSFilterPlugin to support intent_threshold_maps (#196)
    * update: update OOSFilterPlugin to support intent_threshold_maps
    
    Signed-off-by: sanchit-ahuja <sanchitahuja205@gmail.com>
    
    * fix: poetry issue
    
    Signed-off-by: sanchit-ahuja <sanchitahuja205@gmail.com>
    
    * fix: added a warning if both threshold and map init
    
    Signed-off-by: sanchit-ahuja <sanchitahuja205@gmail.com>
    
    ---------
    
    Signed-off-by: sanchit-ahuja <sanchitahuja205@gmail.com>

--- dialogy/plugins/text/oos_filter/__init__.py
+++ dialogy/plugins/text/oos_filter/__init__.py
@@ -10,31 +10,58 @@ from typing import Any, List, Optional, Union
 
 from dialogy.base.plugin import Input, Output, Plugin, Guard
 from dialogy.types import Intent
+from dialogy.utils import logger
+import yaml
 
 
 class OOSFilterPlugin(Plugin):
     def __init__(
         self,
-        threshold: float,
         intent_oos: str,
+        intent_threshold_map_path: Optional[str] = None,
         dest: Optional[str] = None,
+        threshold: Optional[float] = None,
         guards: Optional[List[Guard]] = None,
-        **kwargs: Any
+        **kwargs: Any,
     ) -> None:
         super().__init__(dest=dest, guards=guards, **kwargs)
 
-        if not isinstance(threshold, float):
+        if not isinstance(intent_oos, str):
+            raise TypeError(
+                f"intent_oos type should be string, received {type(intent_oos)}"
+            )
+
+        if threshold and not isinstance(threshold, float):
             raise TypeError(
                 f"Threshold type should be float, received {type(threshold)}"
             )
-        
-        if not isinstance(intent_oos, str):
+
+        if intent_threshold_map_path and not isinstance(intent_threshold_map_path, str):
             raise TypeError(
-                f"Threshold type should be float, received {type(intent_oos)}"
+                f"intent_threshold_map_path type should be string, \
+                received {type(intent_threshold_map_path)}"
+            )
+
+        if not threshold and not intent_threshold_map_path:
+            raise TypeError(
+                "Both threshold and intent_threshold_map_path cannot be empty. \
+                Please give either one."
             )
 
         self.threshold = threshold
         self.intent_oos = intent_oos
+
+        self.intent_threshold_map = None
+        if intent_threshold_map_path:
+            with open(intent_threshold_map_path, "r") as f:
+                self.intent_threshold_map = yaml.safe_load(f)
+
+        if self.threshold and self.intent_threshold_map:
+            self.threshold = None
+            logger.warning(
+                "Both threshold and the intent_threshold_map have been instantiated \
+                The threshold value is being overriden to None."
+            )
 
     def set_oos_intent(self, intents: List[Intent]) -> List[Intent]:
         if not intents:
@@ -42,8 +69,20 @@ class OOSFilterPlugin(Plugin):
                 "Either OOSFilter was added before the intent classifier plugin or the intents list is empty"
             )
 
-        if intents[0].score < self.threshold:
+        intent_name = intents[0].name
+        intent_score = intents[0].score
+
+        if (
+            self.threshold
+            and intent_score < self.threshold
+            or (
+                self.intent_threshold_map
+                and intent_name in self.intent_threshold_map
+                and intent_score < self.intent_threshold_map[intent_name]
+            )
+        ):
             intents[0].name = self.intent_oos
+
         return intents
 
     def utility(self, input_: Input, output: Output) -> List[Intent]:
--- tests/plugin/text/oos_filter/test_oos_filter.py
+++ tests/plugin/text/oos_filter/test_oos_filter.py
@@ -8,32 +8,33 @@ import pytest
 
 @pytest.mark.parametrize("payload", load_tests("oos_filter", __file__))
 def test_oos_filter(payload) -> None:
-    intent_oos = payload["intent_oos"]
-
-    if not isinstance(payload["threshold"], float):
+    threshold = payload.get("threshold")
+    intent_oos = payload.get("intent_oos")
+    intent_threshold_map_path = payload.get("intent_threshold_map_path")
+    if (
+        (threshold is None and intent_threshold_map_path is None)
+        or (threshold is not None and not isinstance(threshold, float))
+        or (intent_oos is not None and not isinstance(intent_oos, str))
+        or (
+            intent_threshold_map_path is not None
+            and not isinstance(intent_threshold_map_path, str)
+        )
+    ):
         with pytest.raises(EXCEPTIONS[payload["exception"]]):
-            oos_filter = OOSFilterPlugin(
+            OOSFilterPlugin(
                 dest="output.intents",
-                threshold=payload["threshold"],
+                threshold=threshold,
                 replace_output=True,
                 intent_oos=intent_oos,
+                intent_threshold_map_path=intent_threshold_map_path,
             )
-    
-    elif not isinstance(payload["intent_oos"], str):
-        with pytest.raises(EXCEPTIONS[payload["exception"]]):
-            oos_filter = OOSFilterPlugin(
-                dest="output.intents",
-                threshold=payload["threshold"],
-                replace_output=True,
-                intent_oos=intent_oos,
-            )
-
     else:
         oos_filter = OOSFilterPlugin(
             dest="output.intents",
-            threshold=payload["threshold"],
+            threshold=threshold,
             replace_output=True,
             intent_oos=intent_oos,
+            intent_threshold_map_path=payload.get("intent_threshold_map_path"),
         )
 
         # Create a mock workflow
@@ -41,7 +42,7 @@ def test_oos_filter(payload) -> None:
         intent_name = payload["original_intent"]
         body = [[{"transcript": payload["transcript"]}]]
         intent = Intent(name=intent_name, score=payload["predicted_confidence"])
-        
+
         if payload["empty_intents"]:
             output = Output(intents=[])
             with pytest.raises(EXCEPTIONS[payload["exception"]]):
