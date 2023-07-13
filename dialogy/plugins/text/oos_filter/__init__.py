87daf250526eb6fb712ef4d00ed1eb8bd253eb75
Daksh Varshneya <dakshvar22@gmail.com>
2023-07-13 21:11:57 +0530

    dont use oos filter plugin during training

--- dialogy/plugins/text/oos_filter/__init__.py
+++ dialogy/plugins/text/oos_filter/__init__.py
@@ -52,7 +52,7 @@ class OOSFilterPlugin(Plugin):
         self.intent_oos = intent_oos
 
         self.intent_threshold_map = None
-        if intent_threshold_map_path:
+        if intent_threshold_map_path and self.purpose != "train":
             with open(intent_threshold_map_path, "r") as f:
                 self.intent_threshold_map = yaml.safe_load(f)
 
